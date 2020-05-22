efce1196929b2608453df46ab51abc5f159d60ce
Paul Hendrick <paul@error.agency>
2020-05-22 20:33:46 +0100

    Initial Commit

---
+++ bibs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bibs
+//
+//  Created by Paul Hendrick on 22/05/2020.
+//  Copyright © 2020 Paul Hendrick. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bibsTests/bibsTests.swift
@@ -0,0 +1,34 @@
+//
+//  bibsTests.swift
+//  bibsTests
+//
+//  Created by Paul Hendrick on 22/05/2020.
+//  Copyright © 2020 Paul Hendrick. All rights reserved.
+//
+
+import XCTest
+@testable import bibs
+
+class bibsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
